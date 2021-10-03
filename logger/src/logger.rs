92660ed1a3cbe42930499340c7cbc6e51936887a
IroniX2 <57399961+IroniX2@users.noreply.github.com>
2021-10-03 11:44:23 +0200

    cleanup logger, remove un-needed code

--- logger/src/logger.rs
+++ logger/src/logger.rs
@@ -1,19 +1,17 @@
-
-
 #[macro_export]
 macro_rules! error {
     ($message:expr) => {
         logger::crate_error!("{}", $message);
     };
-    ($message:expr, $($var:expr)?) => {
-        logger::crate_error!("{:<size$}{:?}", $message, $($var)?, size = SIZE);
+    ($message:expr, $var:expr) => {
+        logger::crate_error!("{:<size$}{:?}", $message, $var, size = SIZE);
     };
 }
 
 #[macro_export]
 macro_rules! warn {
     ($message:expr) => {
-        logger::crate_warn!("{:<size$}", $message, size = SIZE);
+        logger::crate_warn!("{}", $message);
     };
     ($message:expr, $var:expr) => {
         logger::crate_warn!("{:<size$}{:?}", $message, $var, size = SIZE);
@@ -23,7 +21,7 @@ macro_rules! warn {
 #[macro_export]
 macro_rules! info {
     ($message:expr) => {
-        logger::crate_info!("{:<size$}", $message, size = SIZE);
+        logger::crate_info!("{}", $message);
     };
     ($message:expr, $var:expr) => {
         logger::crate_info!("{:<size$}{}", $message, $var, size = SIZE);
